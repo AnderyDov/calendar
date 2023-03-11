48d267e37df29674bfb526d3555d320f47a48f6e
Andrey <dovban.andreyy@rambler.ru>
2023-03-11 04:49:10 +0300

    добавлена перваначальная установка даты, реализовано изменение месяца и года

--- calendar.js
+++ calendar.js
@@ -3,16 +3,34 @@ const icon = document.querySelector('.icon');
 const calendar = document.querySelector('.calendar');
 const numbers = document.querySelectorAll('.number');
 
+const yearVal = document.querySelector('.year-val');
+const monthVal = document.querySelector('.month-val');
+
+const year = document.querySelector('.year');
+const month = document.querySelector('.month');
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+
+const now = new Date();
+
+window.onload = () => {
+    year.value = now.getFullYear();
+    month.value = now.getMonth();
+};
+
+// Скрывает и показывает календарь
 icon.onclick = () => {
     calendar.classList.toggle('hide');
 };
+
+// Скрывает календарь при клике на любой области кроме иконки календаря
 document.body.onclick = (e) => {
-    console.log(e.target);
-    if (!e.target.classList.contains('icon')) {
+    if (!e.target.classList.contains('cl')) {
         calendar.classList.add('hide');
     }
 };
 
+// В цикле вешаются обработчики события фокус и блюр, для изменения ширины бордера в инпуте
 numbers.forEach((el) => {
     el.onfocus = () => {
         input.classList.add('border');
@@ -21,3 +39,53 @@ numbers.forEach((el) => {
         input.classList.remove('border');
     };
 });
+
+//  Обработчик выбора года
+year.oninput = (e) => {
+    const val = e.target.value;
+    if (val.length < 5) {
+        yearVal.innerHTML = val;
+    } else {
+        e.target.value = 2023;
+        yearVal.innerHTML = 2023;
+    }
+};
+
+// Обработчик выбора месяца
+month.onchange = (e) => {
+    let val = e.target.value;
+    console.log(val.length);
+    if (+val > 8) {
+        monthVal.innerHTML = +val + 1;
+    } else {
+        monthVal.innerHTML = '0' + (+val + 1);
+    }
+};
+
+up.onclick = () => {
+    console.log(month.value);
+    if (month.value === '11') {
+        month.value = '0';
+    } else {
+        month.value = +month.value + 1;
+    }
+    if (+month.value > 8) {
+        monthVal.innerHTML = +month.value + 1;
+    } else {
+        monthVal.innerHTML = '0' + (+month.value + 1);
+    }
+};
+
+down.onclick = () => {
+    console.log(month.value);
+    if (month.value === '0') {
+        month.value = '11';
+    } else {
+        month.value = +month.value - 1;
+    }
+    if (+month.value > 8) {
+        monthVal.innerHTML = +month.value + 1;
+    } else {
+        monthVal.innerHTML = '0' + (+month.value + 1);
+    }
+};
